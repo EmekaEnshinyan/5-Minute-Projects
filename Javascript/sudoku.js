83017d9bc82524a4663ef71642cc97f29e54a5d3
Enshinyan <emeka.enshinyan@gmail.com>
2023-10-14 17:07:35 -0700

    add: sudoku solver-uc

---
+++ Javascript/sudoku.js
@@ -0,0 +1,40 @@
+/**
+ * Sudoku Solver
+ * 
+ * Constraints: 
+ * 1. all numbers in a row should be unique
+   2. all numbers in a column should be unique
+   3. all numbers in smaller 3 by 3 square should be unique
+ */
+
+//test board
+let board = [
+  ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+  ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+  [".", "9", "8", ".", ".", ".", ".", "6", "."],
+  ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+  ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+  ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+  [".", "6", ".", ".", ".", ".", "2", "8", "."],
+  [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+  [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+];
+//input random ints into blocks
+let x = Math.floor(Math.random() * 10);
+console.log(x);
+
+GenerateSudokuNum = () => {
+  num = Math.floor(Math.random() * 10);
+  if (num > 10 || num == 0) {
+    return GenerateSudokuNum(num);
+  }
+  console.log(num);
+  return num;
+};
+console.log("random num: ");
+GenerateSudokuNum();
+const col = 9;
+const row = 9;
+
+let grid = Array.from({ length: row }, () => new Array(col).fill(0));
+console.log(grid);
